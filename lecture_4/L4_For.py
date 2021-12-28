dcc107d8586e3bb833971132dfe69e2d8e0cd22f
william-estes <william.estes@estesadvtech.com>
2021-12-27 20:57:24 -0700

    lecture 4

---
+++ lecture_4/L4_For.py
@@ -0,0 +1,36 @@
+for x in range(0, 10):
+    print(x)
+
+print('-------------') # spacing in output
+
+for x in range(10):
+    print(x)
+
+print('-------------')
+
+lst = [0,1,2,3,4]
+for n in lst:
+    print(n)
+
+print('-------------')
+
+tup = (0,1,2,3,4)
+for t in tup:
+    print(t)
+
+print('-------------')
+
+my_dict = {'key1': 'val1', 'key2': 'val2'}
+for k, v in my_dict.items():
+    print(k, v)
+
+print('-------------')
+
+for k in my_dict:
+    print(k)
+
+print('-------------')
+
+for v in my_dict.values():
+    print(v)
+    
